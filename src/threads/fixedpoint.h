bcd06d72a9060166e237b9bfa903e898a5d85c72
Ezra Sitorus <es1419@ic.ac.uk>
2020-10-15 19:10:44 +0100

    [es1419][ss8919] Updated fixpoint.h macros

--- src/threads/fixedpoint.h
+++ src/threads/fixedpoint.h
@@ -1,9 +1,9 @@
 #define P 17
 #define Q 14
-#define f (1 << Q)
+#define F (1 << Q)
 
-#define FIXPOINT (n)(n * f)
-#define ROUNDZERO_INT (x)(x / f)
-#define ROUNDNEAR_INT (x)(x >= 0) ? ((x + f) / 2) : ((x - f) / 2)
-#define ADD_FIXED (x, y)(x + y)
-#define ADD_INT (x, n)(x + n * f)
+#define FIXPOINT(n) (n * F)
+#define ROUNDZERO_INT(x) (x / F)
+#define ROUNDNEAR_INT(x) ((x >= 0) ? (((x + (F / 2)) / F)) : (((x - (F / 2))) / F))
+#define ADD_FIXED(x, y) (x + y)
+#define ADD_INT(x, n) (x + n * F)
