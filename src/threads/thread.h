51456a92be89b6be46f1a55da5c0ad1be423c841
sarveen <ss8919@ic.ac.uk>
2020-10-27 21:39:14 +0000

    [es1419][ss8919][mp2019] Formatted and added thread_set_effective_priority prototype

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -8,22 +8,22 @@
 
 /* States in a thread's life cycle. */
 enum thread_status
-  {
-    THREAD_RUNNING,     /* Running thread. */
-    THREAD_READY,       /* Not running but ready to run. */
-    THREAD_BLOCKED,     /* Waiting for an event to trigger. */
-    THREAD_DYING        /* About to be destroyed. */
-  };
+{
+   THREAD_RUNNING, /* Running thread. */
+   THREAD_READY,   /* Not running but ready to run. */
+   THREAD_BLOCKED, /* Waiting for an event to trigger. */
+   THREAD_DYING    /* About to be destroyed. */
+};
 
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
-#define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
+#define TID_ERROR ((tid_t)-1) /* Error value for tid_t. */
 
 /* Thread priorities. */
-#define PRI_MIN 0                       /* Lowest priority. */
-#define PRI_DEFAULT 31                  /* Default priority. */
-#define PRI_MAX 63                      /* Highest priority. */
+#define PRI_MIN 0      /* Lowest priority. */
+#define PRI_DEFAULT 31 /* Default priority. */
+#define PRI_MAX 63     /* Highest priority. */
 
 /* A kernel thread or user process.
 
@@ -82,75 +82,76 @@ typedef int tid_t;
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
 struct thread
-  {
-    /* Owned by thread.c. */
-    int base_priority;                   /* base priority*/
-    struct list blocked_threads;         /*list of threads that are blocked by this thread*/
-    struct thread* priority_receiver;    /*thread that reacieves priority from this thread*/
-    struct list_elem blocked_elem;       /*List element for blocked_threads list*/
-    tid_t tid;                          /* Thread identifier. */
-    enum thread_status status;          /* Thread state. */
-    char name[16];                      /* Name (for debugging purposes). */
-    uint8_t *stack;                     /* Saved stack pointer. */
-    int effective_priority;             /* efective priority. */
-    struct list_elem allelem;           /* List element for all threads list. */
-    int time_to_wake_up;                /* The time when the thread must wake up if it's asleep*/
-    struct list_elem sleep_elem; 	    /* List element for sleep_list*/ 	
-    struct semaphore sema;              /*A semaphore  that blocks that thread*/ 
-    struct list_elem queue_elem;        /* List element for priority queue */
-    /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
+{
+   /* Owned by thread.c. */
+   int base_priority;                /* base priority*/
+   struct list blocked_threads;      /*list of threads that are blocked by this thread*/
+   struct thread *priority_receiver; /*thread that reacieves priority from this thread*/
+   struct list_elem blocked_elem;    /*List element for blocked_threads list*/
+   tid_t tid;                        /* Thread identifier. */
+   enum thread_status status;        /* Thread state. */
+   char name[16];                    /* Name (for debugging purposes). */
+   uint8_t *stack;                   /* Saved stack pointer. */
+   int effective_priority;           /* efective priority. */
+   struct list_elem allelem;         /* List element for all threads list. */
+   int time_to_wake_up;              /* The time when the thread must wake up if it's asleep*/
+   struct list_elem sleep_elem;      /* List element for sleep_list*/
+   struct semaphore sema;            /*A semaphore  that blocks that thread*/
+   struct list_elem queue_elem;      /* List element for priority queue */
+   /* Shared between thread.c and synch.c. */
+   struct list_elem elem; /* List element. */
 
-    /* Values for BSD scheduler */
+   /* Values for BSD scheduler */
    int nice;       /* The thread's nice value. */
    int recent_cpu; /* Value indicating how much CPU time the thread has used recently */
 
 #ifdef USERPROG
-    /* Owned by userprog/process.c. */
-    uint32_t *pagedir;                  /* Page directory. */
+   /* Owned by userprog/process.c. */
+   uint32_t *pagedir; /* Page directory. */
 #endif
 
-    /* Owned by thread.c. */
-    unsigned magic;                     /* Detects stack overflow. */
-  };
+   /* Owned by thread.c. */
+   unsigned magic; /* Detects stack overflow. */
+};
 
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
    Controlled by kernel command-line option "mlfqs". */
 extern bool thread_mlfqs;
 
-void thread_init (void);
-void thread_start (void);
+void thread_init(void);
+void thread_start(void);
 size_t threads_ready(void);
 
-void thread_tick (void);
-void thread_print_stats (void);
+void thread_tick(void);
+void thread_print_stats(void);
 
-typedef void thread_func (void *aux);
-tid_t thread_create (const char *name, int priority, thread_func *, void *);
+typedef void thread_func(void *aux);
+tid_t thread_create(const char *name, int priority, thread_func *, void *);
 
-void thread_block (void);
-void thread_unblock (struct thread *);
+void thread_block(void);
+void thread_unblock(struct thread *);
 
-struct thread *thread_current (void);
-tid_t thread_tid (void);
-const char *thread_name (void);
+struct thread *thread_current(void);
+tid_t thread_tid(void);
+const char *thread_name(void);
 
-void thread_exit (void) NO_RETURN;
-void thread_yield (void);
+void thread_exit(void) NO_RETURN;
+void thread_yield(void);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
-typedef void thread_action_func (struct thread *t, void *aux);
-void thread_foreach (thread_action_func *, void *);
+typedef void thread_action_func(struct thread *t, void *aux);
+void thread_foreach(thread_action_func *, void *);
 
-int thread_get_priority (void);
-void thread_set_priority (int);
+int thread_get_priority(void);
+void thread_set_priority(int);
 
-int thread_get_nice (void);
-void thread_set_nice (int);
-int thread_get_recent_cpu (void);
-int thread_get_load_avg (void);
-struct thread* highest_priority_thread_elem(struct list*);
-int highest_thread_priority_blocked_elem(struct list*);
+int thread_get_nice(void);
+void thread_set_nice(int);
+int thread_get_recent_cpu(void);
+int thread_get_load_avg(void);
+struct thread *highest_priority_thread_elem(struct list *);
+int highest_thread_priority_blocked_elem(struct list *);
+void thread_set_effective_priority(struct thread *, int);
 
 #endif /* threads/thread.h */
