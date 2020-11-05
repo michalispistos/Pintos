ef4da95573d4c19ad3d34afdb3e5a990f3c3aad8
Ezra Sitorus <es1419@ic.ac.uk>
2020-11-05 19:51:42 +0000

    [es1419][mp2019][crl19] Implemented verify_memory_address
    Co-authored-by: Pistos, Michalis <mp2019@ic.ac.uk>
    Co-authored-by: Lee, Anthony <crl19@ic.ac.uk>

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,18 +3,62 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
-static void syscall_handler (struct intr_frame *);
+typedef int pid_t;
 
-void
-syscall_init (void) 
+static void syscall_handler(struct intr_frame *);
+
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  printf("system call!\n");
+  thread_exit();
+}
+
+// static void halt(void) {}
+
+// static void exit(int status) {}
+
+// static pid_t exec(const char *cmd_line) {}
+
+// static int wait(pid_t pid) {}
+
+// static bool create(const char *file, unsigned initial_size) {}
+
+// static bool remove(const char *file) {}
+
+// static int open(const char *file) {}
+
+// static int filesize(int fd) {}
+
+// static int read(int fd, void *buffer, unsigned size) {}
+
+// static int write(int fd, const void *buffer, unsigned size) {}
+
+// static void seek(int fd, unsigned position) {}
+
+// static unsigned tell(int fd) {}
+
+// static void close(int fd) {}
+
+/* Verifies a given memory address.
+*/
+static bool
+verify_memory_address(struct thread *t, void *user_pointer)
+{
+  if (!user_pointer || !is_user_vaddr(user_pointer) || !pagedir_get_page(t->pagedir, user_pointer))
+  {
+    thread_exit();
+    // TODO: Need to call exit() after implementing it
+    pagedir_destroy(t->pagedir);
+    return false;
+  }
+  return true;
 }
