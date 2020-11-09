140a677e8c98e3a92741550a1d388a0d0cebf031
Ezra Sitorus <es1419@ic.ac.uk>
2020-11-09 22:19:21 +0000

    [es1419][ss8919][crl19][mp2019] Added new struct child_thread_info
    Co-authored-by: Pistos, Michalis <mp2019@ic.ac.uk>
    Co-authored-by: Lee, Anthony <crl19@ic.ac.uk>
    Co-authored-by: Santhiramohan, Sarveen <ss8919@ic.ac.uk>

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -6,11 +6,11 @@
 /* Not neccessarily alive */
 struct child_thread_info
 {
-    struct list_elem tid_elem; /* List element stored in struct thread. */
-    tid_t tid;                 /* Tid of the child. */
-    int exit_code;             /* Exit code of the child thread when terminated by the kernel. */
-    bool has_been_waited_on;   /* True if child thread has been called by process_wait(). */
-    bool has_died;             /* True if it has been terminated by the kernel. */
+    struct list_elem child_elem; /* List element stored in struct thread. */
+    tid_t tid;                   /* Tid of the child. */
+    int exit_code;               /* Exit code of the child thread. */
+    bool has_been_waited_on;     /* True if child thread has been called by process_wait(). */
+    bool exited_normally;        /* True if exit() was called on it. */
 };
 
 tid_t process_execute(const char *file_name);
