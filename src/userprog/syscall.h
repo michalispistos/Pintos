b91508ab682fa2bdd949971d84008edbcd31f718
Ezra Sitorus <es1419@ic.ac.uk>
2020-11-13 17:25:20 +0000

    [es1419][crl19] Made open_file struct

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init (void);
+void syscall_init(void);
+
+struct open_file
+{
+    struct list_elem fd_elem;
+    int fd;
+    struct file *file;
+}
 
 #endif /* userprog/syscall.h */
