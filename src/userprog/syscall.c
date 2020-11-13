13f435a4e5bb33fcffa379b3c320521f0b7f0d35
sarveen <ss8919@ic.ac.uk>
2020-11-13 20:09:42 +0000

    [mp2019][crl19][es1419][ss8919] Finished read syscall. Implemented syscall handler.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -11,6 +11,10 @@
 #include "filesys/filesys.h"
 #include "filesys/file.h"
 #include "threads/palloc.h"
+#include "devices/shutdown.h"
+#include "lib/string.h"
+#include "devices/input.h"
+
 
 typedef int pid_t;
 
@@ -20,6 +24,8 @@ typedef int pid_t;
 /* Lock needed to use the filesystem. */
 static struct lock file_lock;
 
+static struct lock exec_lock;
+
 /* File descriptor for open. */
 static int fd = 2;
 
@@ -28,6 +34,8 @@ static void syscall_handler(struct intr_frame *);
 void syscall_init(void)
 {
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init(&file_lock);
+  lock_init(&exec_lock);
 }
 
 static void halt(void)
@@ -40,20 +48,26 @@ static void
 exit(int status)
 {
   printf("%s: exit(%d)\n", thread_current()->name, status);
-  thread_current()->thread_info->exited_normally = true;
-  thread_current()->thread_info->exit_code = status;
+  struct thread_info *info = thread_current()->thread_info;
+  if (info){
+    info->exited_normally = true;
+    info->exit_code = status;
+  }
   thread_exit();
 }
 
 // TODO: Check this is correct
 static pid_t exec(const char *cmd_line)
 {
-  return process_execute(cmd_line);
+  lock_acquire(&exec_lock);
+  pid_t pid = process_execute(cmd_line);
+  lock_release(&exec_lock);
+  return pid; 
 }
 
 static int wait(pid_t pid)
 {
-  process_wait(pid);
+  return process_wait(pid);
 }
 
 static bool create(const char *file, unsigned initial_size)
@@ -78,18 +92,20 @@ static int open(const char *file)
   struct file *file_to_open = filesys_open(file);
   if (file_to_open == NULL)
   {
+    lock_release(&file_lock);
     return -1;
   }
 
   struct open_file *of = palloc_get_page(PAL_ZERO);
   if (of == NULL)
   {
+    lock_release(&file_lock);
     return TID_ERROR;
   }
 
   of->fd = fd++;
   of->file = file_to_open;
-  list_push_front(thread_current()->open_files, &of->fd_elem);
+  list_push_front(&thread_current()->open_files, &of->fd_elem);
 
   lock_release(&file_lock);
   return of->fd;
@@ -97,7 +113,6 @@ static int open(const char *file)
 
 static int filesize(int fd)
 {
-
   struct list_elem *e;
   struct open_file *of;
   lock_acquire(&file_lock);
@@ -119,11 +134,23 @@ static int filesize(int fd)
 static int read(int fd, void *buffer, unsigned size)
 {
   lock_acquire(&file_lock);
-  // todo
-  // if (fd == STDIN_FILENO)
-  // {
-  //   return input_getc();
-  // }
+  if (fd == STDIN_FILENO)
+  {
+    char* buffer_ = (char*) buffer;
+    uint32_t buffer_length = strlen(buffer_);
+    char word[size];
+    for (uint32_t i = 0; i < size; i++) {             
+      word[i] = input_getc();                        
+    }
+    strlcpy (buffer_, word, size); 
+    if(buffer_length<=size){
+      lock_release(&file_lock);
+      return buffer_length;
+    }else{
+      lock_release(&file_lock);
+      return size;
+    }
+  }
 
   struct list_elem *e;
   struct open_file *of;
@@ -146,6 +173,7 @@ static int read(int fd, void *buffer, unsigned size)
 static int
 write(int fd, const void *buffer, unsigned size)
 {
+  lock_acquire(&file_lock);
   if (fd == STDOUT_FILENO)
   {
     int tracker = 0;
@@ -156,10 +184,10 @@ write(int fd, const void *buffer, unsigned size)
       size -= MAX_SINGLE_BUFFER_SIZE;
     }
     putbuf(buffer + tracker, size);
+    lock_release(&file_lock);
     return size;
   }
 
-  lock_acquire(&file_lock);
   struct list_elem *e;
   struct open_file *of;
   for (e = list_begin(&thread_current()->open_files); e != list_end(&thread_current()->open_files); e = list_next(e))
@@ -188,9 +216,8 @@ static void seek(int fd, unsigned position)
     of = list_entry(e, struct open_file, fd_elem);
     if (of->fd == fd)
     {
-      lock_release(&file_lock);
       file_seek(of->file, position);
-      return;
+      break;
     }
   }
   lock_release(&file_lock);
@@ -254,6 +281,58 @@ static void
 syscall_handler(struct intr_frame *f)
 {
   int syscall_num = *(int *)(f->esp);
-  printf("The syscall number is: %d\n", syscall_num);
-  exit(0);
+  void** argv;
+  int i=0;
+  f->esp = f->esp+4;
+  while(f->esp!=NULL){
+    argv[i] = *(void **)f->esp;
+     i++;
+  }
+  switch (syscall_num){
+    case SYS_HALT:
+      halt();
+      break;
+    case SYS_EXIT:
+      exit((int) argv[0]);
+      break;
+    case SYS_EXEC:
+      verify_memory_address(thread_current(),argv[0]);
+      f->eax = exec((const char *) argv[0]);
+      break;
+    case SYS_WAIT:
+      f->eax = wait((int) argv[0]);
+      break;
+    case SYS_CREATE:
+      verify_memory_address(thread_current(),argv[0]);
+      f->eax = create((const char *) argv[0], (unsigned int) argv[1]);
+      break;
+    case SYS_REMOVE:
+      verify_memory_address(thread_current(),argv[0]);
+      f->eax = remove((const char *) argv[0]);
+      break;
+    case SYS_OPEN:
+      verify_memory_address(thread_current(),argv[0]);
+      f->eax = open((const char *) argv[0]);
+      break;
+    case SYS_FILESIZE:
+      f->eax = filesize((int) argv[0]);
+      break;
+    case SYS_READ:
+      verify_memory_address(thread_current(),argv[1]);
+      f->eax = read((int) argv[0], (void *) argv[1], (unsigned int) argv[2]);
+      break;
+    case SYS_WRITE:
+      verify_memory_address(thread_current(),argv[1]);
+      f->eax = write((int) argv[0], (const void*) argv[1], (unsigned int) argv[2]);
+      break;
+    case SYS_SEEK:
+      seek((int) argv[0], (unsigned int) argv[1]);
+      break;
+    case SYS_TELL:
+      tell((int) argv[0]);
+      break;  
+    case SYS_CLOSE:
+      close((int) argv[0]);
+      break;
+  }
 }
