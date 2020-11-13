bed8a4537a2def6af22c4c40f034a99dc2e2e68d
sarveen <ss8919@ic.ac.uk>
2020-11-13 20:07:40 +0000

    [ss8919] Included threads.h header in syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,3 +1,5 @@
+#include "threads/thread.h"
+
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
@@ -8,6 +10,6 @@ struct open_file
     struct list_elem fd_elem;
     int fd;
     struct file *file;
-}
+};
 
 #endif /* userprog/syscall.h */
