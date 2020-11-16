bcc5a22c7d7453fec37c7ca6b1e4e012d84e3eca
Ezra Sitorus <es1419@ic.ac.uk>
2020-11-16 12:02:19 +0000

    [mp2019][es1419][ss8919] Readded load_failed member in thread_info struct

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -13,6 +13,7 @@ struct thread_info
     bool exited_normally;        /* True if exit() was called on it. */
     struct thread *self;         /* A pointer to a thread with id tid. */
     struct lock lock;            /* A lock to prevent child from dying during process wait */
+    bool load_failed;
     struct semaphore wait_sema;
 };
 
